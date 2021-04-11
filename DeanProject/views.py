8085f7a0f52e782f1b2c49f7160ac4db0da97ac3
Ryne Whitaker <rynewhitaker@pm.me>
2021-04-11 17:51:48 -0500

    Add/drop class form processing, boolean function for unchecked values

--- DeanProject/views.py
+++ DeanProject/views.py
@@ -91,23 +91,19 @@ def processPermitToRegister(request):
       return redirect('/error')
 
 def processAddDropClass(request):
+      db = sqlite3.connect('db.sqlite3')
+      cursor = db.cursor()
+      cursor.execute('''INSERT INTO DeanProject_add_dropclass(student_id_number, date, name_enrolled_under, recieves_financial_aid, financial_aid_representative_signature, total_hours_enrolled_after_change, comments, advisor_signature, student_signature, atu_comments, another_course_fits_schedule, changing_major, changing_minor, classes_too_large, could_not_understand_the_instructor_course_or_materials, course_not_required_for_major, inadequate_academic_support_services, insufficient_high_school_preparation, lack_of_academic_challenge, lack_of_progress_in_the_courses, need_to_re_enroll_in_classes_next_semester, need_to_re_enroll_in_classes_with_different_instructor, quality_of_instruction_did_not_meet_expections, reduce_course_load, wanted_classes_face_to_face, wanted_classes_online, change_in_family_financial_circumstances, didnt_have_enough_money_to_continue, financial_aid_was_not_sufficient, increases_in_tuition_and_fees, incurred_too_much_debt, needed_Course_for_financial_aid_eligibility, scholarship_Grant_was_not_renewed, family_illness_responsibility, homesick, wanted_to_be_closer_to_family_and_friends, commute_too_long, moved_out_of_the_area, distracted_Social_life, felt_class_climate_unwelcoming, felt_out_of_place_in_class, impact_of_natural_disaster, inadequate_study_skills_or_lack_of_academic_success, military_obligations, personal_health, personal_emergency, unmotivated_for_this_courses_or_tired_of_school, working_too_many_hours)
+            VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (request.POST['student_id_number'], request.POST['date'], request.POST['name_enrolled_under'], trySetBool(request, 'recieves_financial_aid'), request.POST['financial_aid_representative_signature'], request.POST['total_hours_enrolled_after_change'], request.POST['comments'], request.POST['advisor_signature'], request.POST['student_signature'], request.POST['atu_comments'], trySetBool(request, 'another_course_fits_schedule'), trySetBool(request, 'changing_major'), trySetBool(request, 'changing_minor'), trySetBool(request, 'classes_too_large'), trySetBool(request, 'could_not_understand_the_instructor_course_or_materials'), trySetBool(request, 'course_not_required_for_major'), trySetBool(request, 'inadequate_academic_support_services'), trySetBool(request, 'insufficient_high_school_preparation'), trySetBool(request, 'lack_of_academic_challenge'), trySetBool(request, 'lack_of_progress_in_the_courses'), trySetBool(request, 'need_to_re_enroll_in_classes_next_semester'), trySetBool(request, 'need_to_re_enroll_in_classes_with_different_instructor'), trySetBool(request, 'quality_of_instruction_did_not_meet_expections'), trySetBool(request, 'reduce_course_load'), trySetBool(request, 'wanted_classes_face_to_face'), trySetBool(request, 'wanted_classes_online'), trySetBool(request, 'change_in_family_financial_circumstances'), trySetBool(request, 'didnt_have_enough_money_to_continue'), trySetBool(request, 'financial_aid_was_not_sufficient'), trySetBool(request, 'increases_in_tuition_and_fees'), trySetBool(request, 'incurred_too_much_debt'), trySetBool(request, 'needed_Course_for_financial_aid_eligibility'), trySetBool(request, 'scholarship_Grant_was_not_renewed'), trySetBool(request, 'family_illness_responsibility'), trySetBool(request, 'homesick'), trySetBool(request, 'wanted_to_be_closer_to_family_and_friends'), trySetBool(request, 'commute_too_long'), trySetBool(request, 'moved_out_of_the_area'), trySetBool(request, 'distracted_Social_life'), trySetBool(request, 'felt_class_climate_unwelcoming'), trySetBool(request, 'felt_out_of_place_in_class'), trySetBool(request, 'impact_of_natural_disaster'), trySetBool(request, 'inadequate_study_skills_or_lack_of_academic_success'), trySetBool(request, 'military_obligations'), trySetBool(request, 'personal_health'), trySetBool(request, 'personal_emergency'), trySetBool(request, 'unmotivated_for_this_courses_or_tired_of_school'), trySetBool(request, 'working_too_many_hours')))
+      db.commit()
+      db.close()
       return redirect('/error')
 
 def processUGGraduation(request):
-      try:
-          if (request.POST['pronunciation_recorded'] == 'on'):
-              pronunciation_recorded = True
-      except:
-          pronunciation_recorded = False
-      try:
-          if (request.POST['parents_completed_bachelor_degree'] == 'on'):
-              parents_completed_bachelor_degree = True
-      except:
-          parents_completed_bachelor_degree = False
       db = sqlite3.connect('db.sqlite3')
       cursor = db.cursor()
       cursor.execute('''INSERT INTO DeanProject_uggraduation(student_id_number, date, name_enrolled_under, phone_number, student_signature, diploma_name, name_pronunciation, pronunciation_recorded, parents_completed_bachelor_degree, expected_graduation_term, expected_graduation_year, preferred_degree)
-            VALUES(?,?,?,?,?,?,?,?,?,?,?,?)''', (request.POST['student_id_number'], request.POST['date'], request.POST['name_enrolled_under'], request.POST['phone_number'], request.POST['student_signature'], request.POST['diploma_name'], request.POST['name_pronunciation'], pronunciation_recorded, parents_completed_bachelor_degree, request.POST['expected_graduation_term'], request.POST['expected_graduation_year'], request.POST['preferred_degree']))
+            VALUES(?,?,?,?,?,?,?,?,?,?,?,?)''', (request.POST['student_id_number'], request.POST['date'], request.POST['name_enrolled_under'], request.POST['phone_number'], request.POST['student_signature'], request.POST['diploma_name'], request.POST['name_pronunciation'], trySetBool(request,'pronunciation_recorded'), trySetBool(request, 'parents_completed_bachelor_degree'), request.POST['expected_graduation_term'], request.POST['expected_graduation_year'], request.POST['preferred_degree']))
       db.commit()
       db.close()
       return redirect('/error')
@@ -122,57 +118,31 @@ def processMasterGraduation(request):
       return redirect('/error')
 
 def processDegreeAudit(request):
-      try:
-          if (request.POST['major_was_chosen'] == 'on'):
-              major_was_chosen = True
-      except:
-          major_was_chosen = False
-      try:
-          if (request.POST['minor_was_chosen'] == 'on'):
-              minor_was_chosen = True
-      except:
-          minor_was_chosen = False
       db = sqlite3.connect('db.sqlite3')
       cursor = db.cursor()
       cursor.execute('''INSERT INTO DeanProject_degreeaudit(student_id_number, catalog_year, name_enrolled_under, major_or_minor_name, major_was_chosen, minor_was_chosen, semester, year)
-            VALUES(?,?,?,?,?,?,?,?)''', (request.POST['student_id_number'], request.POST['catalog_year'], request.POST['name_enrolled_under'], request.POST['major_or_minor_name'], major_was_chosen, minor_was_chosen, request.POST['semester'], request.POST['year']))
-      db.commit()
-      db.close()
-
-def processTranscriptRequest(request):
-      try:
-          if (request.POST['adhe'] == 'on'):
-              adhe = True
-      except:
-          adhe = False
-      try:
-          if (request.POST['sacm'] == 'on'):
-              sacm = True
-      except:
-          sacm = False
-      try:
-          if (request.POST['embassy_of_kuwait'] == 'on'):
-              embassy_of_kuwait = True
-      except:
-          embassy_of_kuwait = False
-      try:
-          if (request.POST['ade_licensure'] == 'on'):
-              ade_licensure = True
-      except:
-          ade_licensure = False
-      try:
-          if (request.POST['arsbn'] == 'on'):
-              arsbn = True
-      except:
-          arsbn = False
-
-      db = sqlite3.connect('db.sqlite3')
-      cursor = db.cursor()
-      cursor.execute('''INSERT INTO DeanProject_transcriptrequest(student_id_number, date, name_enrolled_under, birth_date, mailing_address, city, state, zip_code, phone_number, student_signature, adhe, sacm, embassy_of_kuwait, ade_licensure, arsbn)
-            VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (request.POST['student_id_number'], request.POST['date'], request.POST['name_enrolled_under'], request.POST['birth_date'], request.POST['mailing_address'], request.POST['city'], request.POST['state'], request.POST['zip_code'], request.POST['phone_number'], request.POST['student_signature'], adhe, sacm, embassy_of_kuwait, ade_licensure, arsbn))
+            VALUES(?,?,?,?,?,?,?,?)''', (request.POST['student_id_number'], request.POST['catalog_year'], request.POST['name_enrolled_under'], request.POST['major_or_minor_name'], trySetBool(request, 'major_was_chosen'), trySetBool(request, 'minor_was_chosen'), request.POST['semester'], request.POST['year']))
       db.commit()
       db.close()
       return redirect('/error')
+
+def processTranscriptRequest(request):
+      db = sqlite3.connect('db.sqlite3')
+      cursor = db.cursor()
+      cursor.execute('''INSERT INTO DeanProject_transcriptrequest(student_id_number, date, name_enrolled_under, birth_date, mailing_address, city, state, zip_code, phone_number, student_signature, adhe, sacm, embassy_of_kuwait, ade_licensure, arsbn)
+            VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (request.POST['student_id_number'], request.POST['date'], request.POST['name_enrolled_under'], request.POST['birth_date'], request.POST['mailing_address'], request.POST['city'], request.POST['state'], request.POST['zip_code'], request.POST['phone_number'], request.POST['student_signature'], trySetBool(request, 'adhe'), trySetBool(request, 'sacm'), trySetBool(request, 'embassy_of_kuwait'), trySetBool(request, 'ade_licensure'), trySetBool(request, 'arsbn')))
+      db.commit()
+      db.close()
+      return redirect('/error')
+
+def trySetBool(request, title):
+      try:
+          if (request.POST[title] == 'on'):
+              result = True
+              return result
+      except:
+          result = False
+          return result
 
 def error(request):
     return render(request, 'DeanProject/error.html')
